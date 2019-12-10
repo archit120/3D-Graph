3709de20def323e4c1cff1f8409ef1f5cb55ecd7
aswinigh <aswighosh@gmail.com>
2019-12-10 23:07:20 +0530

    updated renderlinux.h

--- 3D Graph/ControlLoop.cpp
+++ 3D Graph/ControlLoop.cpp
@@ -1,5 +1,51 @@
 #ifdef _WIN32
-#include "stdafx.h"
+	#include "stdafx.h"
+#elif __APPLE__||__linux__
+	#include <SDL2/SDL.h>
+	#include "renderlinux.h"
+	struct POINT
+	{
+		int x,y;
+	};
+
+	void GetCursorPos(POINT *p)
+	{	int x,y;
+		SDL_GetMouseState(&x, &y);
+		p->x=x;
+		p->y=y;
+	}
+	#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+ 
+int _kbhit(void)
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+ 
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+ 
+  ch = getchar();
+ 
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+ 
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return 1;
+  }
+ 
+  return 0;
+}
+ 
 #endif
 #include "ControlLoop.h"
 #include <cmath>
